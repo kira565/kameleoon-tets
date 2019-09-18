7916d711be2193a9f2f34f539d499c7bdecea2bf
kira565 <50900823+kira565@users.noreply.github.com>
2019-09-18 19:57:32 +0300

    Add files via upload

--- js/test__4.js
+++ js/test__4.js
@@ -8,8 +8,7 @@ let randomNum = getRandomFloat(0, 100).toFixed(0);
 function numberCheck() {
     let result = document.getElementById('result');
     let entire = document.getElementById('inp').value;
-
-    console.log(randomNum);
+    
     if (entire < randomNum){
         result.innerHTML = "Too Low"
     } else if (entire > randomNum) {
